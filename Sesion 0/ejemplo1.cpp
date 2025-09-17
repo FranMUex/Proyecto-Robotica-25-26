bf44823cab3577007c674f11dbe1eb285bd141b4
Guilecaso <128077596+Guilecaso@users.noreply.github.com>
2025-09-17 15:52:07 +0200

    Sesion 0

---
+++ Sesion 0/ejemplo1.cpp
@@ -0,0 +1,31 @@
+#include "ejemplo1.h"
+
+ejemplo1::ejemplo1(): Ui_Counter()
+{
+	num = 0;
+	timer = new QTimer(this);
+	setupUi(this);
+	show();
+	connect(button, SIGNAL(clicked()), this, SLOT(doButton()) );
+	connect(timer, &QTimer::timeout, this, &ejemplo1::doCount);
+	timer->start(1000);
+}
+
+void ejemplo1::doButton()
+{
+	qDebug() << "click on button";
+	if (timer->isActive()) {
+		timer->stop();
+	} else {
+		num = 0;
+		timer->start(1000);
+		lcdNumber->display(num);
+	}
+}
+
+void ejemplo1::doCount() {
+	qDebug() << "click on count";
+	lcdNumber->display(num++);
+}
+
+
