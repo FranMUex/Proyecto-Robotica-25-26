dc908cdc8bdae53b77a5eb11d230a8a20a0c6bc8
Francisco Manuel Nieto Mendo <147628690+FranMUex@users.noreply.github.com>
2025-10-09 15:36:36 +0200

    init_script.sh arreglado

--- init_script.sh
+++ init_script.sh
@@ -6,22 +6,13 @@ webots &
 disown
 
 cd ~/robocomp/components/webots-bridge/
-pwd
 
 gnome-terminal --tab --execute bash -c "bin/Webots2Robocomp etc/config ; bash"
 
 cd ~/robocomp/components/robocomp-robolab/components/hardware/laser/lidar3D/
-pwd
 
 gnome-terminal --tab --execute bash -c "bin/Lidar3D etc/config_helios_webots ; bash"
 
 cd ~/robocomp/components/robocomp-robolab/components/hardware/external_control/joystickpublish/
-pwd
 
 gnome-terminal --tab --execute bash -c "bin/JoystickPublish etc/config_shadow ; bash"
-
-cd ~
-
-git clone https://github.com/FranMUex/Proyecto-Robotica-25-26
-
-cd ~/Proyecto-Robotica-25-26/
